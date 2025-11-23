afdb60afabacf0748471d3ca8c255c8bf8d89d9b
dev-bernardofofg <dev.bernardofofg@gmail.com>
2025-11-23 18:46:26 -0300

    refactor: replace direct validation with validatorHelpers in CreateUserController and GetUserByIdController

--- src/controllers/create-user.ts
+++ src/controllers/create-user.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express'
-import validator from 'validator'
 import { EmailAlreadyInUseError } from '../errors/user'
 import { CreateUserParams } from '../repositories/postgres/create-user'
 import { CreateUserUseCase } from '../use-cases/create-user'
+import { validatorHelpers } from '../utils/validators'
 import { responseHelper } from './helpers'
 
 export const CreateUserController = {
@@ -15,34 +15,14 @@ export const CreateUserController = {
       'email',
       'password',
     ]
-    const emailIsValid = validator.isEmail(params.email ?? '')
-    const passwordIsValid = validator.isStrongPassword(params.password ?? '', {
-      minLength: 6,
-      minLowercase: 1,
-      minUppercase: 1,
-      minNumbers: 1,
-      minSymbols: 1,
-    })
+    validatorHelpers.emailIsValid(params.email ?? '', res)
+    validatorHelpers.passwordIsValid(params.password ?? '', res)
 
     for (const field of requiredFields) {
       if (!params?.[field] || String(params[field]).trim().length === 0) {
         // Verifica se o campo é obrigatório e se está vazio
         return responseHelper.badRequest(res, `O campo ${field} é obrigatório`)
       }
-    }
-
-    if (!passwordIsValid) {
-      return responseHelper.badRequest(
-        res,
-        'A senha deve ter pelo menos 6 caracteres, uma letra maiúscula, uma letra minúscula, um número e um símbolo.',
-      )
-    }
-
-    if (!emailIsValid) {
-      return responseHelper.badRequest(
-        res,
-        'O email não é válido. Por favor, informe um email válido.',
-      )
     }
 
     // Executar o use case
--- src/controllers/get-user-by-id.ts
+++ src/controllers/get-user-by-id.ts
@@ -1,20 +1,15 @@
 import { Request, Response } from 'express'
-import validator from 'validator'
 import { UserNotFoundError } from '../errors/user'
 import { GetUserByIdParams } from '../repositories/postgres/get-user-by-id'
 import { GetUserByIdUseCase } from '../use-cases/get-user-by-id'
+import { validatorHelpers } from '../utils/validators'
 import { responseHelper } from './helpers'
 
 export const GetUserByIdController = {
   execute: async (req: Request, res: Response) => {
     const params = req.params as Partial<GetUserByIdParams>
-    const idIsValid = validator.isUUID(params.id ?? '')
-    if (!idIsValid) {
-      return responseHelper.badRequest(
-        res,
-        'O ID não é válido. Por favor, informe um ID válido.',
-      )
-    }
+
+    validatorHelpers.idIsValid(params.id ?? '', res)
 
     try {
       const user = await GetUserByIdUseCase.execute(params as GetUserByIdParams)
