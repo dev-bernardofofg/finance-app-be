6297a84f6e0caae166bb4a1c2dfe516f585f4ae6
dev-bernardofofg <dev.bernardofofg@gmail.com>
2026-02-24 16:43:44 -0300

    feat(database): add get user balance function in PostgreSQL
    - Create a new SQL function to calculate user balance, including earnings, expenses, and investments.
    - The function returns a table with the user's financial summary based on their transactions.
    - This addition supports the recently implemented user balance retrieval functionality.

---
+++ src/db/postgres/migrations/03-add-get-user-balance-function.sql
@@ -0,0 +1,62 @@
+CREATE OR REPLACE FUNCTION get_user_balance(p_user_id UUID) RETURNS TABLE (
+    earnings NUMERIC(10, 2),
+    expenses NUMERIC(10, 2),
+    investments NUMERIC(10, 2),
+    balance NUMERIC(10, 2)
+  ) AS $$ BEGIN RETURN QUERY
+SELECT COALESCE(
+    SUM(
+      CASE
+        WHEN type = 'income' THEN amount
+        ELSE 0
+      END
+    ),
+    0
+  ) AS earnings,
+  COALESCE(
+    SUM(
+      CASE
+        WHEN type = 'expense' THEN amount
+        ELSE 0
+      END
+    ),
+    0
+  ) AS expenses,
+  COALESCE(
+    SUM(
+      CASE
+        WHEN type = 'investment' THEN amount
+        ELSE 0
+      END
+    ),
+    0
+  ) AS investments,
+  COALESCE(
+    SUM(
+      CASE
+        WHEN type = 'income' THEN amount
+        ELSE 0
+      END
+    ),
+    0
+  ) - COALESCE(
+    SUM(
+      CASE
+        WHEN type = 'expense' THEN amount
+        ELSE 0
+      END
+    ),
+    0
+  ) - COALESCE(
+    SUM(
+      CASE
+        WHEN type = 'investment' THEN amount
+        ELSE 0
+      END
+    ),
+    0
+  ) AS balance
+FROM transactions
+WHERE user_id = p_user_id;
+END;
+$$ LANGUAGE plpgsql;
