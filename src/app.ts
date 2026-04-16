e87d5a0dd73de099ad4980e58e01a0f95f6b2084
dev-bernardofofg <dev.bernardofofg@gmail.com>
2026-04-16 15:15:15 -0300

    feat(api): integrate Swagger UI for API documentation
    - Added Swagger UI Express to serve API documentation.
    - Created a new swagger.json file to define API metadata.
    - Updated app.ts to include Swagger UI setup and serve documentation at the /docs endpoint.
    - Updated package.json and pnpm-lock.yaml to include necessary dependencies for Swagger UI.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import express from 'express'
+import fs from 'fs'
 import morgan from 'morgan'
+import path from 'path'
+import swaggerUi from 'swagger-ui-express'
 import { routes } from './routes'
 
 export const app = express()
+
 app.use(express.json())
 app.use(morgan('dev'))
 
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(
+    path.join(import.meta.dirname, '..', 'docs', 'swagger.json'),
+    'utf8',
+  ),
+)
+
 app.use(routes)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
