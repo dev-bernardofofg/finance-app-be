c66df0f92099550b245f474be515193c0a8c141d
dev-bernardofofg <dev.bernardofofg@gmail.com>
2026-04-16 00:58:55 -0300

    feat(tests): expand user E2E tests to cover additional scenarios
    - Added tests for PATCH and DELETE user routes to handle cases where the user is not found, returning appropriate 404 responses.
    - Implemented tests for user deletion when transactions exist, ensuring successful deletion and correct balance retrieval through the GET /users/balance/:id route.
    - Included a test for handling 404 responses when querying balance for a non-existent user.

--- src/routes/users.e2e.test.ts
+++ src/routes/users.e2e.test.ts
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker/.'
 import request from 'supertest'
+import { TransactionType } from '../../generated/prisma/client'
 import { app } from '../app'
+import { transactionFixtureWithoutId } from '../test/fixtures/transaction'
 import { userFixtureWithoutId } from '../test/fixtures/user'
 
 describe('User Routes E2E Tests', () => {
@@ -82,6 +84,15 @@ describe('User Routes E2E Tests', () => {
     expect(response.status).toBe(400)
   })
 
+  it('PATCH /users/:id should return 404 when the user is not found', async () => {
+    const response = await request(app)
+      .patch(`/users/${faker.string.uuid()}`)
+      .send({
+        ...userFixtureWithoutId,
+      })
+    expect(response.status).toBe(404)
+  })
+
   it('DELETE /users/:id should return 200 when the user is deleted successfully', async () => {
     const createUserResponse = await request(app)
       .post('/users')
@@ -95,6 +106,74 @@ describe('User Routes E2E Tests', () => {
 
   it('DELETE /users/:id should return 404 when the user is not found', async () => {
     const response = await request(app).delete(`/users/${faker.string.uuid()}`)
+    expect(response.status).toBe(404)
+  })
+
+  it('DELETE /users/:id should return 200 when the user have transactions', async () => {
+    const createUserResponse = await request(app)
+      .post('/users')
+      .send({
+        ...userFixtureWithoutId,
+      })
+    const userId = createUserResponse.body.id
+    await request(app)
+      .post('/transactions')
+      .send({
+        ...transactionFixtureWithoutId,
+        user_id: userId,
+        type: TransactionType.INCOME,
+      })
+    const response = await request(app).delete(`/users/${userId}`)
+    expect(response.status).toBe(200)
+  })
+
+  it('GET /users/balance/:id should return 200 when the user have transactions', async () => {
+    const createUserResponse = await request(app)
+      .post('/users')
+      .send({
+        ...userFixtureWithoutId,
+      })
+    const userId = createUserResponse.body.id
+
+    await request(app)
+      .post('/transactions')
+      .send({
+        ...transactionFixtureWithoutId,
+        user_id: userId,
+        type: TransactionType.INCOME,
+        amount: 100,
+      })
+    await request(app)
+      .post('/transactions')
+      .send({
+        ...transactionFixtureWithoutId,
+        user_id: userId,
+        type: TransactionType.EXPENSE,
+        amount: 100,
+      })
+    await request(app)
+      .post('/transactions')
+      .send({
+        ...transactionFixtureWithoutId,
+        user_id: userId,
+        type: TransactionType.INVESTMENT,
+        amount: 100,
+      })
+
+    const response = await request(app).get(`/users/${userId}/balance`)
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({
+      total_income: 100,
+      total_expenses: 100,
+      total_investments: 100,
+      balance: -100,
+    })
+  })
+
+  it('GET /users/balance/:id should return 404 when the user is not found', async () => {
+    const response = await request(app).get(
+      `/users/${faker.string.uuid()}/balance`,
+    )
     expect(response.status).toBe(404)
   })
 })
